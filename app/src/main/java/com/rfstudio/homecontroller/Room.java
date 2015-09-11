c6f75ca86767368f4e2e2e033412bdffb4ee2fff
raveenrox <raveenrox@gmail.com>
2015-09-11 15:16:30 +0530

    added task list

--- app/src/main/java/com/rfstudio/homecontroller/Room.java
+++ app/src/main/java/com/rfstudio/homecontroller/Room.java
@@ -2,9 +2,6 @@ package com.rfstudio.homecontroller;
 
 import java.util.List;
 
-/**
- * Created by Raveen on 8/22/2015.
- */
 public class Room {
     public String name;
     public String image;
--- app/src/main/java/com/rfstudio/homecontroller/Task.java
+++ app/src/main/java/com/rfstudio/homecontroller/Task.java
@@ -2,9 +2,6 @@ package com.rfstudio.homecontroller;
 
 import java.util.List;
 
-/**
- * Created by Raveen on 9/11/2015.
- */
 public class Task {
     public String taskName;
     public String taskDate;
