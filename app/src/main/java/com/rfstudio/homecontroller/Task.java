4083e93189f020929ff0a1643bc91e6b438b8e30
raveenrox <raveenrox@gmail.com>
2015-09-11 15:01:28 +0530

    added task list

---
+++ app/src/main/java/com/rfstudio/homecontroller/Task.java
@@ -0,0 +1,15 @@
+package com.rfstudio.homecontroller;
+
+import java.util.List;
+
+/**
+ * Created by Raveen on 9/11/2015.
+ */
+public class Task {
+    public String taskName;
+    public String taskDate;
+    public String taskTime;
+    public List<TaskCommand> taskCommands;
+}
+
+
